837f32ab6eef1ad0182802708873a666300b6836
hermlon <dev.hermlon@yamx.net>
2018-06-06 21:32:38 +0200

    removed test code

--- rsa.py
+++ rsa.py
@@ -31,22 +31,39 @@ def find_next_prime(random_number):
         if g:
             random_number=random_number+1
             Prim= find_all_primes(random_number)
-        else:
-                
+        else:   
             return random_number
 
 def euklid_ext(a, b):
     """Gibt für Primzahlen a und b mit ggT(a, b) = 1 das Tupel (x, y) zurück, für die a * x + b * y = 1 gilt"""
-    
-    pass
+    cnt_a = 0
+    cnt_b = 1
+    old_cnt_a = 1
+    old_cnt_b = 0
+
+    while b != 0:
+        rest = a % b
+        q = a // b
+        aux_cnt_a = cnt_a
+        aux_cnt_b = cnt_b
+
+        cnt_a = q * cnt_a + old_cnt_a
+        cnt_b = q * cnt_b + old_cnt_b
+
+        old_cnt_a = aux_cnt_a
+        old_cnt_b = aux_cnt_b
+        
+        a = b
+        b = rest
+    # print('{} , {}'.format(old_cnt_a, old_cnt_b))
+    return old_cnt_b
 
 def generate_key(p, q):
     """Diese Funktion generiert aus den Primzahlen p und q den privaten Schlüssel (d, n) und den öffentlichen Schlüssel (e, n) und gibt diese als Tupel (priv_key, pub_key) also ((d, n), (e, n)) zurück"""
     n = p * q
     euler_n = (p-1)*(q-1)
     e = 7
-    """euklid_ext(euler_n, e)"""
-    d = 55
+    d = euklid_ext(euler_n, e)
     priv_key = (d, n)
     pub_key = (e, n)
     return (priv_key, pub_key)
@@ -72,18 +89,21 @@ def decrypt(priv_key, cipher):
         m = c ** d % n
         message += chr(m)
     return message
-"""
+
 # Hauptprogramm
 
 # Bereich in dem die generierte Zufallszahl liegen soll
-keylength = 10 * 100
+keylength = 100
 
 # generiert eine Kryptosichere Zufallszahl und sucht die nächste Primzahl
 p = find_next_prime(secrets.randbelow(keylength))
 q = find_next_prime(secrets.randbelow(keylength))
 
-"""
-keys = generate_key(13, 17)
+print('{}, {}'.format(p, q))
+p = 31
+q = 13
+
+keys = generate_key(p, q)
 
 priv_key = keys[0]
 pub_key = keys[1]
