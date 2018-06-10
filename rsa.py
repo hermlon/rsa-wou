a7b8c7c53c3a5f4a3532bfbc24da9691044324c5
hermlon <dev.hermlon@yamx.net>
2018-06-10 15:12:27 +0200

    negative b in euklid_ext und schnellere Primzahlgenerierung

--- rsa.py
+++ rsa.py
@@ -1,11 +1,48 @@
 import secrets
 import math
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+def find_primes(start, end, known_primes):
+    n = start
+    # nächste ungerade Zahl
+    if n % 2 == 0:
+        n += 1
+        
+    while n <= math.sqrt(end):
+        for p in known_primes:
+            if n % p == 0:
+                break
+        else:
+            known_primes.append(n)
+        n += 2
+    return known_primes
+
+def find_next_prime(a):
+    """Diese Funktion gibt die nächst größere Primzahl zu einer Zahl zurück (und berechnet alle bekannten Primzahlen nicht jedes mal neu)"""
+    # nächste ungerade Zahl
+    if a % 2 == 0:
+        a += 1
+        
+    primes = find_primes(3, a, [2])
+    
+    while True:
+        primes = find_primes(a, a+2, primes)
+        for p in primes:
+            if a % p == 0:
+                break
+        else:
+            return a
+        a += 2
 
 def find_all_primes(random_number):
     Prim = [2]
     i= 0
     a=3
-    while a < math.sqrt(random_number):
+    # kleiner gleich, da sonst Quadratzahlen Primzahlen sind
+    while a <= math.sqrt(random_number):
         f = a % Prim[i]
         if f == 0:
             a = a+1
@@ -18,10 +55,9 @@ def find_all_primes(random_number):
             else:
                 i = i+1
     return Prim
-                
-def find_next_prime(random_number):
-    """Diese Funktion gibt die nächst größere Primzahl zu einer Zahl zurück"""
-    
+
+def find_next_prime_slow(random_number):
+    """Diese Funktion gibt die nächst größere Primzahl zu einer Zahl zurück (und berechnet alle bekannten Primzahlen jedes mal neu)"""
     while True:
         g = False
         Prim= find_all_primes(random_number)
@@ -31,13 +67,13 @@ def find_next_prime(random_number):
         if g:
             random_number=random_number+1
             Prim= find_all_primes(random_number)
-        else:   
+        else:
             return random_number
 
 def euklid_ext(a, b):
-    u_a = a
-    u_b = b
     """Gibt für Primzahlen a und b mit ggT(a, b) = 1 das Tupel (x, y) zurück, für die a * x + b * y = 1 gilt"""
+    org_a = a
+        
     cnt_a = 0
     cnt_b = 1
     old_cnt_a = 1
@@ -46,36 +82,32 @@ def euklid_ext(a, b):
     while b != 0:
         rest = a % b
         q = a // b
-        aux_cnt_a = cnt_a
-        aux_cnt_b = cnt_b
 
-        cnt_a = q * cnt_a + old_cnt_a
-        cnt_b = q * cnt_b + old_cnt_b
-
-        old_cnt_a = aux_cnt_a
-        old_cnt_b = aux_cnt_b
+        old_cnt_a, cnt_a = cnt_a, -q * cnt_a + old_cnt_a
+        old_cnt_b, cnt_b = cnt_b, -q * cnt_b + old_cnt_b
         
         a = b
         b = rest
-    print('{} , {}'.format(old_cnt_a, old_cnt_b))
-    
-    if u_a*old_cnt_a-u_b*old_cnt_b > 0:
-        return old_cnt_b % u_b
+
+    if old_cnt_b < 0:
+        # b ist negativ, das positive modulo a gleiche Ergebnis ist b + a
+        return old_cnt_b + org_a
     else:
-        print('soos')
         return old_cnt_b
-    print(u_a*old_cnt_a-u_b*old_cnt_b)
-    print(u_b*old_cnt_b-u_a*old_cnt_a)
-    
 
 def generate_key(p, q):
     """Diese Funktion generiert aus den Primzahlen p und q den privaten Schlüssel (d, n) und den öffentlichen Schlüssel (e, n) und gibt diese als Tupel (priv_key, pub_key) also ((d, n), (e, n)) zurück"""
     n = p * q
     euler_n = (p-1)*(q-1)
-    e = 7
+    # vierte Fermat Zahl -> schnelle Potenzierung
+    e = 2 ** 16 + 1
     d = euklid_ext(euler_n, e)
     priv_key = (d, n)
     pub_key = (e, n)
+    
+    logging.debug('Privater Schlüssel: d: {}, n: {}'.format(d, n))
+    logging.debug('Öffentlicher Schlüssel: e: {}, n: {}'.format(e, n))
+    
     return (priv_key, pub_key)
 
 def encrypt(pub_key, message):
@@ -87,7 +119,8 @@ def encrypt(pub_key, message):
         m = ord(character)
         c = m ** e % n
         cipher.append(c)
-
+        
+    logging.debug('Verschlüsselte Nachricht: {}'.format(cipher))
     return cipher
 
 def decrypt(priv_key, cipher):
@@ -98,26 +131,33 @@ def decrypt(priv_key, cipher):
     for c in cipher:
         m = c ** d % n
         message += chr(m)
+        
+    logging.debug('Entschlüsselte Nachricht: {}'.format(message))
     return message
+
+def random_key(keylength):
+    """Gibt einen zufällig generierten RSA key mit keylength < Zufallszahl für Primzahlfindung < keylength * 2 zurück"""
+    p = 0
+    q = 0
+    # unterschiedliche Primzahlen
+    while p == q:
+        # generiert eine Kryptosichere Zufallszahl und sucht die nächste Primzahl
+        p = find_next_prime(secrets.randbelow(keylength)+keylength)
+        q = find_next_prime(secrets.randbelow(keylength)+keylength)
+    logging.debug('Zufällige Primzahlen p: {}, q: {}'.format(p, q))
+    return generate_key(p, q)
+
 
 # Hauptprogramm
 
-# Bereich in dem die generierte Zufallszahl liegen soll
-keylength = 100
+# zufälliges Schlüsselpaar generieren
+keypair = random_key(300)
 
-# generiert eine Kryptosichere Zufallszahl und sucht die nächste Primzahl
-p = find_next_prime(secrets.randbelow(keylength))
-q = find_next_prime(secrets.randbelow(keylength))
+priv_key = keypair[0]
+pub_key = keypair[1]
 
-print('{}, {}'.format(p, q))
-p = 31
-q = 17
+# verschlüsseln:
+cipher = encrypt(pub_key, 'Eine Testnachricht mit Einhorn und schwebenden Businessman! \U0001F984 \U0001F574')
 
-keys = generate_key(p, q)
-
-priv_key = keys[0]
-pub_key = keys[1]
-
-
-cipher = encrypt(pub_key, 'Test_1')
+# entschlüsseln
 print(decrypt(priv_key, cipher))
